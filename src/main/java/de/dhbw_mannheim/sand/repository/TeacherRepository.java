670291d75c8b8edaf95003194510526372dad38e
FallenAzrael <michaelmueller94@gmx.de>
2015-11-17 14:08:24 +0100

    implemted TeacherService added TeacherServiceImpl, TecherRepository, TeacherService

---
+++ src/main/java/de/dhbw_mannheim/sand/repository/TeacherRepository.java
@@ -0,0 +1,17 @@
+package de.dhbw_mannheim.sand.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import de.dhbw_mannheim.sand.model.Role;
+import de.dhbw_mannheim.sand.model.Student;
+import de.dhbw_mannheim.sand.model.User;
+
+public interface TeacherRepository extends JpaRepository<Student,Integer> {
+	
+	@Query("select r from Teacher r where r.user = ?1")
+	List<Role> findByUser(User user);
+	
+}
