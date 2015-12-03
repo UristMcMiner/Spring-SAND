62bc7e057ada5497a1eb1a4b127c1b2169be95bb
fbaier <florian.baier@she.net>
2015-12-03 23:49:44 +0100

    Added Permissions for ResearchProjectOfferController

---
+++ src/main/java/de/dhbw_mannheim/sand/aspects/authorization/ResearchProjectOfferControllerAuthorizationChecker.java
@@ -0,0 +1,77 @@
+package de.dhbw_mannheim.sand.aspects.authorization;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import de.dhbw_mannheim.sand.model.LazyObject;
+import de.dhbw_mannheim.sand.model.ResearchProjectOffer;
+import de.dhbw_mannheim.sand.model.User;
+import de.dhbw_mannheim.sand.repository.ResearchProjectOfferRepository;
+
+public class ResearchProjectOfferControllerAuthorizationChecker implements AuthorizationChecker{
+	
+	@Autowired
+	private ResearchProjectOfferRepository repository;
+	
+	@Override
+	public boolean checkGetById(User user, int id) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean checkAdd(User user, LazyObject object) {
+		return(user.isAdmin() || user.isStudent() || user.isTeacher());
+	}
+
+	@Override
+	public boolean checkUpdate(User user, LazyObject object) {
+		//If user is Admin allow action
+		if(user.isAdmin()) return true;
+		
+		ResearchProjectOffer pendingChange = (ResearchProjectOffer)object;
+		ResearchProjectOffer existing = repository.getOne(pendingChange.getId());
+		List<User> listBefore = existing.getUsers();
+		List<User> listAfter  = pendingChange.getUsers();
+		Iterator<User> it_a = listAfter.iterator();
+		Iterator<User> it_b = listBefore.iterator();
+		
+		//If User is Creator, allow change
+		if(existing.getCreator().equals(user)) return true;
+		
+		//Only Users who are Students or Teacher may add themselves as interested
+		while(it_a.hasNext()){
+			User u = it_a.next();
+			if(!listBefore.contains(u)){
+				if(u.isTeacher() || u.isStudent()) return true;
+			}
+		}
+		
+		//Only Users who are Students or Teacher may remove themselves from interested
+		while(it_b.hasNext()){
+			User u = it_b.next();
+			if(!listAfter.contains(u)){
+				if(u.isTeacher() || u.isStudent()) return true;
+			}
+		}
+		
+		
+		return false;
+	}
+
+	@Override
+	public boolean checkDelete(User user, int id) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean checkOther(User user, String method, Object param) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+
+}
