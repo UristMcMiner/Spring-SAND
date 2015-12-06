c4c3e1a6a003cb743b569cd604e59e082202ab7f
hendrikluetjen <hendrik.luetjen@icloud.com>
2015-12-06 23:56:40 +0100

    PostService - erste Versuche

--- src/main/java/de/dhbw_mannheim/sand/service/PostServiceImpl.java
+++ src/main/java/de/dhbw_mannheim/sand/service/PostServiceImpl.java
@@ -2,14 +2,31 @@ package de.dhbw_mannheim.sand.service;
 
 import java.util.List;
 
+import javax.annotation.Resource;
+import javax.ejb.EJB;
+import javax.ejb.Local;
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+import javax.sql.DataSource;
+
 import org.apache.log4j.Logger;
 
 import de.dhbw_mannheim.sand.model.Post;
 
+@Stateless
+@LocalBean
+@Local(PostService.class)
 public class PostServiceImpl implements PostService {
 	
 	private Logger logger = Logger.getLogger(PostServiceImpl.class);
 
+	@Resource(name= "sand")
+	private DataSource ds;
+	
+	public PostServiceImpl() {
+		
+	}
+	
 	@Override
 	public Post getPostById(int id) {
 		// TODO Auto-generated method stub
