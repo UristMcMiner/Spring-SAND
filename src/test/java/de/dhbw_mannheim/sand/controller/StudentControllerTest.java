209cc03a6669092403f70673a54ba893071ffc8a
maximixk <max.klingelstein@yahoo.de>
2015-12-13 02:07:07 +0100

    Fertig + getestet

--- src/test/java/de/dhbw_mannheim/sand/controller/StudentControllerTest.java
+++ src/test/java/de/dhbw_mannheim/sand/controller/StudentControllerTest.java
@@ -1,10 +1,6 @@
 package de.dhbw_mannheim.sand.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 import javax.transaction.Transactional;
@@ -32,6 +28,14 @@ import com.fasterxml.jackson.databind.ObjectWriter;
 
 import de.dhbw_mannheim.sand.SAND;
 import de.dhbw_mannheim.sand.model.Login;
+import de.dhbw_mannheim.sand.model.Student;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SAND.class)
@@ -75,14 +79,174 @@ public class StudentControllerTest {
 		result.andDo(print());
 		result.andExpect(status().isOk());
 		result.andExpect(jsonPath("$.login").value("Rainer_Colgen"));
-		getRequest = get("/sand/student/9999");
+		getRequest = get("/sand/students/9999");
 		getRequest.header("authorization", authorization);
 		result = mvc.perform(getRequest);
 		result.andExpect(status().isNotFound());
-		getRequest = get("/sand/student/5");
+		getRequest = get("/sand/students/5");
 		getRequest.header("authorization", authorization);
 		result = mvc.perform(getRequest);
 		result.andDo(print());
 		result.andExpect(status().isOk());
+	}        
+        
+        @Test
+	public void testAdd() throws Exception {
+		Login login = new Login();
+		login.setLogin("Rainer_Colgen");
+		login.setPassword("Rainer_Colgen");
+		ObjectMapper mapper = new ObjectMapper();
+		PrettyPrinter printer = new DefaultPrettyPrinter();
+		ObjectWriter writer = mapper.writer().with(printer);
+		String json = writer.writeValueAsString(login);
+		MockHttpServletRequestBuilder postRequest = post("/sand/session/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json);
+		MvcResult result0= mvc.perform(postRequest).andReturn();
+		String object = (String) result0.getResponse().getContentAsString();
+		System.out.println(object);
+		int finish = object.indexOf("user")-3;
+		String authorization = object.substring(7, finish);
+		System.out.println("AUTH: "+authorization);
+		
+		//generate Student JSON
+		String content = "{\"type\" : \"student\","
+				+ "\"id\" : 2,"
+				+ "\"user\" : {\"id\": 2,"
+				+ "\"deleted\" : 0,"
+				+ "\"roles\" : [ ]},"
+				+ "\"startDate\" : \"1990-08-12\","
+				+ "\"endDate\" : \"2998-12-31\","
+				+ "\"enrollmentNumber\" : \"66555\"}";
+		
+		postRequest = post("/sand/students/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(content);
+		postRequest.header("authorization", authorization);
+		ResultActions result;
+		result = mvc.perform(postRequest);
+		result.andDo(print());
+		result.andExpect(status().isCreated());
+		result.andExpect(jsonPath("$.user.login").value("Joachim_Schmidt"));
+	}
+        @Test
+	public void testDelete() throws Exception {
+		Login login = new Login();
+		login.setLogin("Rainer_Colgen");
+		login.setPassword("Rainer_Colgen");
+		ObjectMapper mapper = new ObjectMapper();
+		PrettyPrinter printer = new DefaultPrettyPrinter();
+		ObjectWriter writer = mapper.writer().with(printer);
+		String json = writer.writeValueAsString(login);
+		MockHttpServletRequestBuilder postRequest = post("/sand/session/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json);
+		MvcResult result0= mvc.perform(postRequest).andReturn();
+		String object = (String) result0.getResponse().getContentAsString();
+		System.out.println(object);
+		int finish = object.indexOf("user")-3;
+		String authorization = object.substring(7, finish);
+		System.out.println("AUTH: "+authorization);
+		
+		//generate Student JSON
+		String content = "{\"type\" : \"student\","
+				+ "\"id\" : 2,"
+				+ "\"user\" : {\"id\": 2,"
+				+ "\"deleted\" : 0,"
+				+ "\"roles\" : [ ]},"
+				+ "\"startDate\" : \"1990-08-12\","
+				+ "\"endDate\" : \"2998-12-31\","
+				+ "\"enrollmentNumber\" : \"66555\"}";
+		
+		postRequest = post("/sand/students/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(content);
+		postRequest.header("authorization", authorization);
+		ResultActions result;
+		result = mvc.perform(postRequest);
+		
+		//delete Student
+		MockHttpServletRequestBuilder deleteRequest = delete("/sand/students/" + mapper.readValue(result.andReturn().getResponse().getContentAsString(), Student.class).getId())
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(content);
+		deleteRequest.header("authorization", authorization);
+		result = mvc.perform(deleteRequest);
+		result.andDo(print());
+		result.andExpect(status().isOk());
+		result.andExpect(content().string("true"));
+	}
+        
+        @Test
+	public void testEdit() throws Exception {
+		Login login = new Login();
+		login.setLogin("Rainer_Colgen");
+		login.setPassword("Rainer_Colgen");
+		ObjectMapper mapper = new ObjectMapper();
+		PrettyPrinter printer = new DefaultPrettyPrinter();
+		ObjectWriter writer = mapper.writer().with(printer);
+		String json = writer.writeValueAsString(login);
+		MockHttpServletRequestBuilder postRequest = post("/sand/session/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(json);
+		MvcResult result0= mvc.perform(postRequest).andReturn();
+		String object = (String) result0.getResponse().getContentAsString();
+		System.out.println(object);
+		int finish = object.indexOf("user")-3;
+		String authorization = object.substring(7, finish);
+		System.out.println("AUTH: "+authorization);
+		
+		//generate Student JSON
+		String content = "{\"type\" : \"student\","
+				+ "\"id\" : 2,"
+				+ "\"user\" : {\"id\": 2,"
+				+ "\"deleted\" : 0,"
+				+ "\"roles\" : [ ]},"
+				+ "\"startDate\" : \"1990-08-12\","
+				+ "\"endDate\" : \"2998-12-31\","
+				+ "\"enrollmentNumber\" : \"66555\"}";
+		
+		postRequest = post("/sand/students/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(content);
+		postRequest.header("authorization", authorization);
+		ResultActions result;
+		result = mvc.perform(postRequest);
+		
+		//edit Student JSON
+		content = "{\"type\" : \"student\","
+				+ "\"id\" : 2,"
+				+ "\"user\" : {\"id\": 2,"
+				+ "\"deleted\" : 0,"
+				+ "\"roles\" : [ ]},"
+				+ "\"startDate\" : \"1990-08-12\","
+				+ "\"endDate\" : \"2998-12-31\","
+				+ "\"enrollmentNumber\" : \"66556\"}";
+		
+		MockHttpServletRequestBuilder putRequest = put("/sand/students/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(content);
+		putRequest.header("authorization", authorization);
+		result = mvc.perform(putRequest);
+		result.andDo(print());
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.enrollmentNumber").value("66556"));
+		
+		//try editing non existing Student
+		content = "{\"type\" : \"student\","
+				+ "\"id\" : 9999,"
+				+ "\"user\" : {\"id\": 9999,"
+				+ "\"deleted\" : 0,"
+				+ "\"roles\" : [ ]},"
+				+ "\"startDate\" : \"1990-08-12\","
+				+ "\"endDate\" : \"2998-12-31\","
+				+ "\"enrollmentNumber\" : \"66556\"}";
+		
+		putRequest = put("/sand/students/")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(content);
+		putRequest.header("authorization", authorization);
+		result = mvc.perform(putRequest);
+		result.andDo(print());
+		result.andExpect(status().isConflict());
 	}
 }
